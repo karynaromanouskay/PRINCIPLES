e9fbfabc821675e27883a2b198e55c7863331d3a
karina <karynaromanouskaya@gmail.com>
2024-07-16 18:39:06 +0300

    src/

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    public String name;          // поле для имени
+    public String passport;      // поле для пасспорта
+    public String patronymic;    // поле для отчества
+    public String phone;         // поле для телефона
+    public String surname;       // поле для фамилии
+    public boolean subscription; // поле получения скидок
+    public FormDate birthday;
+}
